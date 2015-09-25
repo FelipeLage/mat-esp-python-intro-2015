4826ed82a7fd78138dbb7ce580ba2dd10458d964
Gabriela Prates <gabi.oceanografia@gmail.com>
2015-09-25 12:14:56 -0300

    adicionamos o camando para gerar uma imagem de gráfico a cada troca

--- andre-felipe-gabriela/bubble-sort-1.py
+++ andre-felipe-gabriela/bubble-sort-1.py
@@ -26,13 +26,31 @@ print ("Lista original: ", cartas)
 #if indica condicao de posicionamento dos elementos, indicando uma acao a ser efetuada, com determinada condicao. 
 #No caso desse programa, a troca da posicao do elemento de acordo com seu valor.
 #temp significa uma posicao temporaria do elemento, para que a troca ocorra de forma continua.
+
+
+a = 0
 for i in range(0, N - 1, 1):
     for j in range (i + 1, N, 1): 
         if cartas[i] > cartas[j]:
             temp = cartas[i]
             cartas[i] = cartas[j]
             cartas[j] = temp
+            
+            plt.figure()
 
+            plt.plot(range(N), cartas, 'ok')
+
+            plt.title("grafico da lista ordenada")
+
+            plt.xlabel("posicao na lista")
+
+            plt.ylabel("valores da lista")
+
+            a = a + 1
+
+            plt.savefig("bubble-troca {}.png" .format(a))
+
+            plt.close()
 #print plota o resultado do logica de programacao, e colocada fora da identacao para que seja rodada apenas uma vez, no final da operacao.
 print("Lista em Ordem Crescente: ", cartas)
 #
